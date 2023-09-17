ed2ce6fd6fc76e6fde229402498759d2b12113d7
Wataru.Kasahara <kasahara1019@gmail.com>
2023-09-17 13:04:07 +0900

    feat: Cloudflare Workers にリクエストする

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private readonly http = inject(HttpClient);
   title = 'hello-clp';
+
+  async ngOnInit(): Promise<void> {
+    const res = await firstValueFrom(this.http.get(environment.apiOrigin));
+    console.log(res);
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiOrigin: 'https://hello-clw.kasaharu.workers.dev/',
+};
